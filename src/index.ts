18af0dea38090fb070e3ed610c2aa682bcca7cdc
Anartz Mugika Ledo <amugika@atsistemas.com>
2023-03-02 12:13:59 +0100

    refactor: object properties

--- src/index.ts
+++ src/index.ts
@@ -13,43 +13,12 @@ import fs from 'fs';
 import path from 'path';
 
 import shelljs from 'shelljs';
+import { ObjectProperties } from './object-properties';
 
 /////////////////////////////////////////////////////////////////////////////////////
 // Object properties (./object-properties/index)
 
-export class ObjectProperties {
-  // Extract value from specific path
-  static getObjectProperty = (object: any, path: string) => {
-    if (object == null) {
-      // null or undefined
-      return object;
-    }
-    const parts = path.split('.');
-    for (let i = 0; i < parts.length; ++i) {
-      if (object == null) {
-        // null or undefined
-        return undefined;
-      }
-      const key = parts[i];
-      object = object[key];
-    }
-    return object;
-  };
 
-  // To use in convert CSV to JSON
-
-  // Use to asign load value in specific path in object
-  static setObjectProperty = (object: any, path: string, value: any) => {
-    const parts = path.split('.');
-    const limit = parts.length - 1;
-    for (let i = 0; i < limit; ++i) {
-      const key = parts[i];
-      object = object[key] ?? (object[key] = {});
-    }
-    const key = parts[limit];
-    object[key] = value;
-  };
-}
 
 
 //////////////////////////////////////////////////////////////////////////////////////
